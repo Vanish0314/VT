aebfda8f7c40a571bd1910155020524822071202
vanishxiao <2013107081@qq.com>
2025-01-12 00:07:41 +0800

    创建VkInstance

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "vulkan/vulkan_core.h"
 #include <iostream>
 #include <exception>
 #include <memory>
@@ -45,14 +46,55 @@ private:
         m_window = glfwCreateWindow(m_width, m_height, m_title, nullptr, nullptr);
     }
     void InitVulkan()
-    {
-
+    {   
+        CreateInstance();
     }
-private:
+    void CreateInstance()
+    {
+        SetVkAppInfo();
+        SetVkInstanceCreateInfo();
+        SetVkInstance();
+    }
+    void SetVkAppInfo()
+    {
+        m_vkAppInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        m_vkAppInfo.pApplicationName = "Hello Triangle";
+        m_vkAppInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        m_vkAppInfo.pEngineName = "No Engine";
+        m_vkAppInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        m_vkAppInfo.apiVersion = VK_API_VERSION_1_0;
+    }
+    void SetVkInstanceCreateInfo()
+    {
+        m_vkCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        m_vkCreateInfo.pApplicationInfo = &m_vkAppInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        m_vkCreateInfo.enabledExtensionCount = glfwExtensionCount;
+        m_vkCreateInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        m_vkCreateInfo.enabledLayerCount = 0;
+    }
+    void SetVkInstance()
+    {
+        if (vkCreateInstance(&m_vkCreateInfo, nullptr, &m_vkInstance) != VK_SUCCESS) 
+        {
+            throw std::runtime_error("[HelloTriangleApplication.class]failed to create instance!");
+        }
+    }
+private: //Window
     unsigned short m_width{1920};
     unsigned short m_height{1080};
     const char *m_title{"VulkanTriangle"};
-    GLFWwindow* m_window; // 裸指针注意
+    GLFWwindow* m_window; // WARN 裸指针注意
+private:
+    VkInstance m_vkInstance;
+    VkApplicationInfo m_vkAppInfo;
+    VkInstanceCreateInfo m_vkCreateInfo;
+
 };
 
 
--- xmake.lua
+++ xmake.lua
@@ -7,6 +7,7 @@ add_rules("mode.debug", "mode.release")
 -- Requirements
 add_requires("glfw3")
 add_requires("glm")
+add_requires("vulkan")
 add_requires("vulkan-headers")
 add_requires("vulkan-loader")
 
@@ -15,6 +16,7 @@ target("HelloVkTriangle")
     add_files("src/*.cpp")
     add_packages("glfw3")
     add_packages("glm")
+    add_packages("vulkan")
     add_packages("vulkan-headers")
     add_packages("vulkan-loader")
 --
